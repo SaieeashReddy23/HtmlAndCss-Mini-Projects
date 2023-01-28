083e5c2185186de2aa5f7b3a92449cf352862353
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-01-28 08:12:04 +0530

    sidebar done

--- 06-sidebar/starter/app.js
+++ 06-sidebar/starter/app.js
@@ -1,17 +1,17 @@
 const getElement = (selector) => {
-  const el = document.querySelector(selector)
-  if (el) return el
-  throw new Error(`Please check your classes : ${selector} does not exist`)
-}
+  const el = document.querySelector(selector);
+  if (el) return el;
+  throw new Error(`Please check your classes : ${selector} does not exist`);
+};
 
-const sidebarToggle = getElement('.sidebar-toggle')
-const sidebar = getElement('.sidebar')
-const closeBtn = getElement('.close-btn')
+const sidebarToggle = getElement(".sidebar-toggle");
+const sidebar = getElement(".sidebar");
+const closeBtn = getElement(".close-btn");
 
-sidebarToggle.addEventListener('click', function () {
-  sidebar.classList.toggle('show-sidebar')
-})
+sidebarToggle.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
 
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('show-sidebar')
-})
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
