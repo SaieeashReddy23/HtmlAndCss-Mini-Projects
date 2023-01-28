94b09a2a25044e42a23efc74a2fb71d88048ee30
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-01-28 09:11:39 +0530

    modal done

--- 07-modal/starter/app.js
+++ 07-modal/starter/app.js
@@ -1,16 +1,19 @@
 const getElement = (selector) => {
-  const el = document.querySelector(selector)
-  if (el) return el
-  throw new Error(`Please double check the ${selector}`)
-}
+  const el = document.querySelector(selector);
+  if (el) return el;
+  throw new Error(`Please double check the ${selector}`);
+};
 
-const modalBtn = getElement('.modal-btn')
-const modal = getElement('.modal-overlay')
-const closeBtn = getElement('.close-btn')
+const modalBtn = getElement(".modal-btn");
+const modal = getElement(".modal-overlay");
+const closeBtn = getElement(".close-btn");
+const hero = getElement(".hero");
 
-modalBtn.addEventListener('click', function () {
-  modal.classList.add('open-modal')
-})
-closeBtn.addEventListener('click', function () {
-  modal.classList.remove('open-modal')
-})
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+  hero.classList.add("light-background");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+  hero.classList.remove("light-background");
+});
